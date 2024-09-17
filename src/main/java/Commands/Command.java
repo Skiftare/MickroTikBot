f64265576931d7de6fe3015f35092055b48d0eb7
Artem <ar.berdiich@ya.ru>
2024-09-18 00:07:50 +0500

    First Chat-Bot Test

--- src/main/java/Commands/Command.java
+++ src/main/java/Commands/Command.java
@@ -1,7 +1,9 @@
 package Commands;
 
-
+import Configuration.TelegramBotConfig;
 
 public interface Command {
-
+    // Метод для выполнения команды. Принимает объект бота и ID чата
+    void execute(TelegramBotConfig bot, long chatId);
 }
+
