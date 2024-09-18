98e4a7810b7a7dd17b9e9ae03514e7a8be52ff97
Kirill Bratanov <kirill.i.b@yandex.ru>
2024-09-18 09:47:28 +0500

    первичная настройка тестов и репозитория

--- src/main/java/Commands/Command.java
+++ src/main/java/Commands/Command.java
@@ -1,5 +1,6 @@
 package Commands;
 
+<<<<<<< HEAD
 import Configuration.TelegramBotConfig;
 
 public interface Command {
@@ -7,3 +8,10 @@ public interface Command {
     void execute(TelegramBotConfig bot, long chatId);
 }
 
+=======
+
+
+public interface Command {
+
+}
+>>>>>>> da20164 (первичная настройка тестов и репозитория)
