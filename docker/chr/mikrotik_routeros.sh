9028b39f0c85459670178f229dd91aefe90e46b2
Berdichevski <ar.berdiich@ya.ru>
2024-09-30 20:21:04 +0500

    попытки в centos -> mikrotikos - 2

--- docker/chr/mikrotik_routeros.sh
+++ docker/chr/mikrotik_routeros.sh
@@ -33,7 +33,3 @@ qemu-system-x86_64 -m "$MEMORY" -smp "$CPUS" -enable-kvm \
     -netdev user,id=net0,hostfwd=tcp::2222-:22, hostfwd=tcp::8291-:8291 -device e1000,netdev=net0 \
     -nographic \
     -boot d
-
-# 5. Возврат IP для SSH соединения
-HOST_IP=$(hostname -I | awk '{print $1}')
-echo "$HOST_IP" > /output/container_ip.txt
