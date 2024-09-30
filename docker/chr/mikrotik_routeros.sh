7ffbcc1fa9864625096ef89976c2361cac27234e
Berdichevski <ar.berdiich@ya.ru>
2024-09-30 22:04:05 +0500

    попытки в centos -> mikrotikos - 2

--- docker/chr/mikrotik_routeros.sh
+++ docker/chr/mikrotik_routeros.sh
@@ -1,35 +1,22 @@
 #!/bin/bash
 
-# Проверяем и устанавливаем зависимости, если их нет
-yum install -y qemu-kvm qemu-img wget unzip
+# Обновление списка пакетов и установка зависимостей
+apt update -y
+apt install -y qemu-kvm qemu-utils wget unzip
 
-# Константы
-ROUTEROS_ISO_URL="https://download.mikrotik.com/routeros/7.10.2/chr-7.10.2.img.zip"
-ROUTEROS_ZIP="/tmp/chr-routeros.zip"
-ROUTEROS_IMG="/tmp/chr-routeros.img"
-DISK_IMG="/opt/mikrotik-routeros-disk.qcow2"
-DISK_SIZE="2G"
-MEMORY="512M"
-CPUS="1"
+# Создание рабочей директории
+mkdir -p /opt/routeros
+cd /opt/routeros
 
-# 1. Скачивание образа MikroTik RouterOS
-if [ ! -f "$ROUTEROS_IMG" ]; then
-    wget -O "$ROUTEROS_ZIP" "$ROUTEROS_ISO_URL"
-    # 2. Распаковка образа
-    unzip "$ROUTEROS_ZIP" -d /tmp/
-    # Удаление zip-файла после распаковки
-    rm -f "$ROUTEROS_ZIP"
-fi
+# Скачиваем образ RouterOS (укажите актуальную версию)
+wget https://download.mikrotik.com/routeros/6.49.6/chr-6.49.6.img.zip
 
-# 3. Создание виртуального диска для RouterOS, если его нет
-if [ ! -f "$DISK_IMG" ]; then
-    qemu-img create -f qcow2 "$DISK_IMG" "$DISK_SIZE"
-fi
+# Распаковываем образ
+unzip chr-6.49.6.img.zip
 
-# 4. Запуск виртуальной машины с MikroTik RouterOS
-qemu-system-x86_64 -m "$MEMORY" -smp "$CPUS" -enable-kvm \
-    -drive file="$DISK_IMG",format=qcow2 \
-    -drive file="$ROUTEROS_IMG",format=raw \
-    -netdev user,id=net0,hostfwd=tcp::2222-:22, hostfwd=tcp::8291-:8291 -device e1000,netdev=net0 \
-    -nographic \
-    -boot d
+# Запуск RouterOS с помощью QEMU
+qemu-system-x86_64 -hda chr-6.49.6.img -boot d -m 256M -nographic \
+  -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22
+
+# Ожидание завершения работы
+echo "RouterOS запущен. Подключитесь через SSH на порт 2222"
