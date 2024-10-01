abb834df1409f3b6fecd7f15cd48a85c4a7d2535
Kirill Bratanov <kirill.i.b@yandex.ru>
2024-10-01 14:23:42 +0500

    Небольшие правки, убран ненужный тестовый функционал

--- migrations/tables_configuration/users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id      BIGINT GENERATED ALWAYS AS IDENTITY,
-    tg_user_id BIGINT PRIMARY KEY NOT NULL,
-    phone VARCHAR(20),
-    name VARCHAR(255) NOT NULL,
-    vpn_profile TEXT,
-    expired_at DATE
-)
--- src/main/java/edu/BotApplication.java
+++ src/main/java/edu/BotApplication.java
@@ -1,8 +1,6 @@
 package edu;
 
-import edu.Configuration.DataConnection;
 import edu.Configuration.TelegramBotCore;
-import edu.Data.DataManager;
 import edu.handles.commands.Command;
 import edu.handles.commands.enteties.*;
 import edu.handles.tables.CommandTable;
@@ -16,23 +14,15 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class BotApplication {
     private static Logger logger = Logger.getLogger(BotApplication.class.getName());
 
-    private static DataConnection dataConnectionAssembling() {
-        logger.info("Assembling data connection");
-        DataConnection dataConnection = new DataConnection();
 
-        logger.info("Data connection assembled");
-        return dataConnection;
-    }
-
-    private static CommandTable commandTableAssembling(DataManager dataManager) {
+    private static CommandTable commandTableAssembling() {
         logger.info("Assembling command table");
 
         Command infoCommand = new InfoCommand();
         Command authorsCommand = new AuthorsCommand();
 
-        Command registerCommand = new RegisterCommand(dataManager);
         Command stateCommand = new StateCommand();
-        CommandTable preCommandTable = new CommandTable(infoCommand, authorsCommand, registerCommand, stateCommand);
+        CommandTable preCommandTable = new CommandTable(infoCommand, authorsCommand, stateCommand);
 
         Command helpCommand = new HelpCommand(preCommandTable);
         logger.info("Command table assembled");
@@ -47,9 +37,7 @@ public class BotApplication {
         try {
             // Сборка бота
             //TODO: стоит ли всё в одном try хранить, или лучше растащить процесс сборки и получше прологировать его?
-            DataConnection dataConnection = dataConnectionAssembling();
-            DataManager dataManager = new DataManager(dataConnection);
-            CommandTable coreCommandTable = commandTableAssembling(dataManager);
+            CommandTable coreCommandTable = commandTableAssembling();
             logger.info("Registering bot");
             botsApi.registerBot(new TelegramBotCore(coreCommandTable));
             logger.info("Bot registered");
