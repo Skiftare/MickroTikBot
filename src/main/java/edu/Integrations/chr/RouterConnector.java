789a61b9cf5f5845c8793c23011b8e9d2d33317e
Berdichevski <ar.berdiich@ya.ru>
2024-11-10 16:12:15 +0500

    пробный период с рекламой. начало.

--- src/main/java/edu/Integrations/chr/RouterConnector.java
+++ src/main/java/edu/Integrations/chr/RouterConnector.java
@@ -72,16 +72,99 @@ public class RouterConnector {
             session.connect();
             LOGGER.info("Подключение для создания ключа успешно установлено.");
 
-            // Создаем канал для выполнения команды
-            ChannelExec channel = (ChannelExec) session.openChannel("exec");
+            final String useProfile = "30d";
 
-            // Команда для выполнения на Mikrotik
-            String command = "/ppp secret add profile=user-profile name=" + finalLogin + " password=" + finalPass + " service=l2tp";
+
+            // Команды для выполнения на Mikrotik
+            String command = "/tool user-manager user create-and-activate-profile \"" + finalLogin + "\" customer=admin " +
+                    "profile=" + useProfile;
+
+            ChannelExec channel = (ChannelExec) session.openChannel("exec");
             LOGGER.info(command);
             channel.setCommand(command);
-            channel.connect();
 
-            // Закрываем канал и сессию
+            // Подключаем канал, выполняем команду и закрываем канал
+            channel.connect();
+            Thread.sleep(TIMEOUT);  // Даем время на выполнение команды
+            channel.disconnect();
+            session.disconnect();
+
+            String result = "VPN профиль успешно создан!\n" +
+                    "Адрес VPN-сервера: " + SERVER_IP + "\n" +
+                    "\nLogin for l2tp: " + finalLogin + "\n\nPassword for l2tp: " + finalPass+
+                    "\n\nSecret: vpn";
+            return result;
+
+        } catch (Exception e) {
+            stateString.append("Не удалось установить соединение! Ошибка: ").append(e.getMessage());
+            LOGGER.severe("Ошибка: " + e);
+            return stateString.toString();
+        }
+    }
+
+    // Метод для инициализации секретного ключа (вызывается из другого класса)
+    public static String initialisationTrial(ClientTransfer clientTransfer) {
+        StringBuilder stateString = new StringBuilder();
+        String finalLogin = "";
+        String finalPass = "";
+        Long userId = clientTransfer.tgUserId();
+        try {
+            if (USER == null || PASSWORD == null) {
+                throw new IllegalStateException("NEW_ROUTER_LOGIN или NEW_ROUTER_PASS не установлены");
+            }
+
+            // Создаем сессию SSH
+            JSch jsch = new JSch();
+            Session session = jsch.getSession(USER, HOST, PORT);
+            session.setPassword(PASSWORD);
+
+            // Отключаем проверку хоста
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.setConfig("PreferredAuthentications", "publickey,password");
+
+            LOGGER.info("Попытка подключения для создания ключа...");
+
+            final String loginCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                    + "abcdefghijklmnopqrstuvwxyz";
+            final String passwordCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                    + "abcdefghijklmnopqrstuvwxyz"
+                    + "0123456789@#!%";
+
+            StringBuilder loginSuffix = new StringBuilder(5);
+
+            Random random = new Random();
+            for (int i = 0; i < 5; i++) {
+                int index = random.nextInt(loginCharacters.length());
+                loginSuffix.append(loginCharacters.charAt(index));
+            }
+
+            StringBuilder password = new StringBuilder(10);
+
+            for (int i = 0; i < 10; i++) {
+                int index = random.nextInt(passwordCharacters.length());
+                password.append(passwordCharacters.charAt(index));
+            }
+
+            finalLogin = userId + "_" + loginSuffix;
+            finalPass = password.toString();
+
+            // Устанавливаем соединение
+            session.connect();
+            LOGGER.info("Подключение для создания ключа успешно установлено.");
+
+            final String useProfile = "1d";
+
+            // Команды для выполнения на Mikrotik
+            String command = "/tool user-manager user create-and-activate-profile \"" + finalLogin + "\" customer=admin " +
+                    "profile=" + useProfile;
+
+            ChannelExec channel = (ChannelExec) session.openChannel("exec");
+            LOGGER.info(command);
+            channel.setCommand(command);
+
+            // Подключаем канал, выполняем команду и закрываем канал
+            channel.connect();
+            Thread.sleep(TIMEOUT);  // Даем время на выполнение команды
             channel.disconnect();
             session.disconnect();
 
